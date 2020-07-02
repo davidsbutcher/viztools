358505c4051ec464d047dd053e9f156388f9416e
David S Butcher <drdavidbutcher@gmail.com>
2020-07-01 23:41:41 -0400

    Add save file func to Shiny app

--- shiny/viztools/app.R
+++ shiny/viztools/app.R
@@ -2,6 +2,15 @@
 library(shiny)
 library(viztools)
 library(tools)
+library(purrr)
+library(ggplot2)
+library(dplyr)
+library(UpSetR)
+library(waffle)
+library(glue)
+library(assertthat)
+library(scales)
+library(forcats)
 
 # Define UI
 ui <- fluidPage(
@@ -34,12 +43,18 @@ ui <- fluidPage(
                 )
             ),
             actionButton("startButton", "Go"),
-            downloadButton("downloadPDF", label = "Download PDF")
+            br(), br(),
+            downloadButton("downloadPDF", label = "Download PDF"),
+            downloadButton("downloadPNG", label = "Download PNG"),
+            downloadButton("downloadSVG", label = "Download SVG")
         ),
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("outputPlot"),
-            plotOutput("res1")
+            plotOutput(
+                "outputPlot",
+                width = 800,
+                height = 500
+            )
         )
 
     )
@@ -51,12 +66,6 @@ server <- function(input, output) {
     observeEvent(
         input$startButton,
         {
-            output$res1 <-
-                renderPrint(
-                    {
-                        input$type
-                    }
-                )
 
             {
                 inFile <- input$file1
@@ -85,37 +94,11 @@ server <- function(input, output) {
 
                 outplot <-
                     plotter(inputsheet)
-                }
+            }
 
             output$outputPlot <-
                 renderPlot(
-                    {
-                        inFile <- input$file1
-
-                        if (file_ext(inFile$datapath) == "csv") {
-
-                            inputsheet <-
-                                readr::read_csv(inFile$datapath)
-
-                        } else if (file_ext(inFile$datapath) == "xlsx") {
-
-                            inputsheet <-
-                                readxl::read_xlsx(inFile$datapath)
-
-                        }
-
-                        plotter <-
-                            switch(
-                                input$type,
-                                upset = make_UpSet_plot,
-                                intdeg = make_intersection_degree_plot,
-                                heatmap = make_heatmap,
-                                waffle = waffle_iron,
-                                make_UpSet_plot
-                            )
-
-                        plotter(inputsheet)
-                    }
+                    outplot
                 )
 
 
@@ -129,6 +112,38 @@ server <- function(input, output) {
                             height = 5,
                             bg = "transparent",
                             useDingbats = FALSE
+                        )
+                        print(outplot)
+                        dev.off()
+                    }
+                )
+
+            output$downloadPNG <-
+                downloadHandler(
+                    filename = "plot.png",
+                    content = function(file) {
+                        png(
+                            file = file,
+                            width = 8,
+                            height = 5,
+                            units = "in",
+                            bg = "white",
+                            res = 300
+                        )
+                        print(outplot)
+                        dev.off()
+                    }
+                )
+
+            output$downloadSVG <-
+                downloadHandler(
+                    filename = "plot.svg",
+                    content = function(file) {
+                        svg(
+                            file = file,
+                            width = 8,
+                            height = 5,
+                            bg = "transparent"
                         )
                         print(outplot)
                         dev.off()
